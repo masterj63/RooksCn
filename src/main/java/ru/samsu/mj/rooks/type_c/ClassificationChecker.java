c85f9c56dfde9d3ab3ee3351c6eb68736eec9506
Igor B <masterj63@gmail.com>
2015-11-03 01:52:37 +0400

    i am to review this commit later due to that failure

--- src/main/java/ru/samsu/mj/rooks/type_c/ClassificationChecker.java
+++ src/main/java/ru/samsu/mj/rooks/type_c/ClassificationChecker.java
@@ -1,11 +1,46 @@
 package ru.samsu.mj.rooks.type_c;
 
-import java.util.List;
+import java.util.*;
+
+import static java.util.Arrays.fill;
 
 class ClassificationChecker {
-    static boolean check(List<byte[]> boardsList, List<Integer>[] sort) {
-        //TODO to implement
-        return false;
+    private final Board[] boards;
+    private final Set<Integer>[] invSort;
+    private final Map<Board, Integer> boardIndMap;
+
+    private ClassificationChecker(List<byte[]> boardsList, List<Integer>[] sort) {
+        final int n = boardsList.size();
+
+        boards = new Board[boardsList.size()];
+        for (int i = 0; i < n; i++)
+            boards[i] = new Board(boardsList.get(i));
+
+        invSort = Sorter.inverseSort(sort);
+
+        boardIndMap = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            boardIndMap.put(boards[i], i);
     }
 
+    static boolean checkGrad(List<byte[]> boardsList, List<Integer>[] sort) {
+        return false;//ClassificationChecker checker = new ClassificationChecker(boardsList, sort);
+    }
+
+    private static boolean rookIsGreater(int a, int b, int c, int d) {
+        return a > c && b < d;
+    }
+
+    private Set<Integer> getL(int ind) {
+        return invSort[ind];
+    }
+
+    private class BytePair {
+        final byte b0, b1;
+
+        public BytePair(int b0, int b1) {
+            this.b0 = (byte) b0;
+            this.b1 = (byte) b1;
+        }
+    }
 }
